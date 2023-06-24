aeac9c60f254c6b016a7007ccb0f2907ef9ceda0
Mohamed Nourin <mohamed0nourin@gmail.com>
2023-06-24 17:26:40 -0600

    Finished

---
+++ week1/assessment/assessment-js-basics-1/gitInfo.js
@@ -0,0 +1,77 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+var gitDefinition = "git is an open source version control system that Developers use to track changes in code during software development. It also provides a way to manage and organize code revisions, enabling teams to efficiently and effectively keep track of all the progress."
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+var gitHubDefinition = "GitHub is  is a web-based platform that provides a user-friendly interface for hosting and managing Git repositories. It allows individuals and teams to store, collaborate on, and track changes to their code projects, facilitating version control and continuous collaboration."
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+var gitInitDefinition = "git init is a command used in Git to initialize a new repository. It creates a new empty Git repository in the current directory. This means that Git will start tracking changes to files within that directory, allowing you to start version controlling your project and recording its history."
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+var gitCloneDefinition = " git clone is a command used in Git to create a copy of a remote repository onto your local machine. It downloads all the files and commits from the remote repository and sets up a local copy that you can work with, allowing you to make changes, view the history, and collaborate on the project."
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+var gitStatusDefinition = "git status is a command used in Git to show the current state of your repository, displaying information about which files have been modified, added, or deleted, and indicating if they are staged for commit or not."
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+var gitAddDefinition = "git add is is a command used in Git to include changes made to files in the staging area, indicating that you want to include those changes in the next commit."
+var gitAddCode = "git add ."
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+var gitCommitDefinition = "git commit is is a command used in Git to save the changes that have been made to the files, creating a new commit with a unique identifier that represents a snapshot of the project at that specific point in time."
+const gitCommitCode = "git commit -m 'initial commit'"
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+var gitPushDefinition = "git push is a command in Git that uploads and synchronizes the local commits with a remote repository, allowing you to share your changes and collaborate with others. "
---
+++ week1/assessment/assessment-js-basics-1/sdlc.js
@@ -0,0 +1,34 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+//Answer: 
+// 1- Analysis of Requirements 
+// 2- Planning
+// 3- Design
+// 4- Implementation
+// 5- Testing & Integration
+// 6- Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const analysisOfRequirments = "This is the very first step in the sdlc, in which the requierments are looked at first. It is very important because we always work according to the bussniess requirments."
+const planning = "This is the second step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const design = "This is the third step, in which the software is being designed, structured, and defined. This step also includes creating detailed technical specifications, user interface designs, and database designs."
+const implementation = "This is the fourth step, in which  software system is developed, tested, and deployed. It involves transforming the design specifications and requirements into actual working software.  the development team writes code, creates software components, integrates modules, and performs unit testing to ensure the individual pieces of the software are functioning correctly.  "
+const testingAndIntegration = "This is the fifth step, in which we do testing dedicated to verifying and validating the software system. It includes various testing activities such as unit testing, integration testing, system testing, and acceptance testing. The goals of this step is to make sure that the software meets the specified requirements in the fisrt step, and that the software functions correctly."
+const maintain = "This is the sixth and last step, in which the software is being maintained.This phase involves the ongoing support and enhancement of the software. It includes activities like bug fixing, updates, performance optimization, and addressing user feedback. This step ensures the software remains reliable, secure, and up-to-date."
+
